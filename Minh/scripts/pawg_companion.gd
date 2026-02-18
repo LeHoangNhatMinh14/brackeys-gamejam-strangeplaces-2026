d42bd311b5b092b90e533fcbc5e11b83640111ce
LeMinhJames23 <202632124+LeHoangNhatMinh14@users.noreply.github.com>
2026-02-18 15:24:25 +0100

    Dog platform unfinished

--- Minh/scripts/pawg_companion.gd
+++ Minh/scripts/pawg_companion.gd
@@ -6,10 +6,10 @@ extends Node2D
 func _ready() -> void:
 	dog.player = get_parent()
 
-func _process(_delta: float) -> void:
-	if not dog.visible:
-		return
-	dog.set_extending(Input.is_action_pressed("dog_extend"))
+#func _process(_delta: float) -> void:
+	#if not dog.visible:
+		#return
+	#dog.set_extending(Input.is_action_pressed("dog_extend"))
 
 func activate_dog() -> void:
 	dog.visible = true
--- Minh/scripts/pete.gd
+++ Minh/scripts/pete.gd
@@ -1,25 +1,43 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var visuals: Node2D = $Visuals
+@onready var bodySprite: AnimatedSprite2D = $Visuals/Body
+@onready var faceSprite: AnimatedSprite2D = $Visuals/Face
+@onready var dogFace: AnimatedSprite2D = $Dog/Visuals/Head/Face
+
+var facing_dir: int = 1
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+
+	# update facing when player presses left/right
+	if direction != 0:
+		facing_dir = -1 if direction < 0 else 1
+		visuals.scale.x = facing_dir  # +1 right, -1 left
+
+	# movement
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	# animations
+	if is_on_floor():
+		bodySprite.play("Idle" if direction == 0 else "Run")
+
+	faceSprite.play("FaceTalk" if Input.is_action_pressed("interact") else "FaceIdle")
+	dogFace.play("Talk" if Input.is_action_pressed("interact") else "Idle")
+
 	move_and_slide()
+
+func get_facing_dir() -> int:
+	return facing_dir
